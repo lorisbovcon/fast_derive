ed86a540d82da6d4c9d69641a75b1edf0450621b
lorisbovcon <loris.bovcon0@gmail.com>
2024-11-17 20:42:16 +0100

    a_few_new_lines

--- cpp_derive.cpp
+++ cpp_derive.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define MAX_EQUATION_LENGTH 128
+#define MAX_EQUATION_SEGMENTS 16
 using namespace std;
 
 int main(int argc, char *argv[]){
@@ -8,7 +9,12 @@ int main(int argc, char *argv[]){
   cout << "Enter the equation of max length " << MAX_EQUATION_LENGTH << ": ";
   cin >> inputed_equation;
 
-  char segmented_equation;
+  char segmented_equation[MAX_EQUATION_SEGMENTS][MAX_EQUATION_LENGTH];
+  int segmented_i;
+  for (segmented_i = 0; (segmented_i < MAX_EQUATION_SEGMENTS) && (int(inputed_equation[segmented_i]) != 0); ++segmented_i) {
+  
+  }
+
 
   return 0;
 }
